8b947815c37b5f8366968df5bba78bba17f46a38
gilasher5103 <gilasher5103@gmail.com>
2026-04-26 21:16:25 +0300

    changed ids patient list

--- src/components/PatientMultiSelect.tsx
+++ src/components/PatientMultiSelect.tsx
@@ -22,7 +22,13 @@ interface PatientMultiSelectProps {
     onChange: (ids: string[]) => void;
 }
 
-const AVAILABLE_PATIENTS = Array.from({ length: 21 }, (_, i) => (1000 + i).toString());
+const AVAILABLE_PATIENTS = [
+    "1", "10", "100", "101", "102", "103", "104", "1000", "1001", "1002", "1003", 
+    "1004", "1005", "1006", "1007", "1008", "1009", "1010", "1011", "1012", "1013", 
+    "1014", "1015", "1016", "1017", "1018", "1019", "1020", "1021", "1022", "1023", 
+    "1024", "1025", "1026", "1027", "1028", "1029", "1030", "1031", "1032", "1033", 
+    "1034", "1035", "1036", "1037", "1038", "1039", "1040", "1041", "1042"
+];
 
 export function PatientMultiSelect({ selectedIds, onChange }: PatientMultiSelectProps) {
     const [open, setOpen] = useState(false);

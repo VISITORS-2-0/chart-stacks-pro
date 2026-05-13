1dc2a6a8bb67b8df55a435d8605b9e32f9e223a3
n0klol <noamkol66@gmail.com>
2026-05-13 09:22:50 +0300

    FIX Fixed race condition when accessing environment variables

--- api/utils/db.ts
+++ api/utils/db.ts
@@ -1,15 +1,15 @@
 import { MongoClient, Db } from 'mongodb';
 
-const uri = process.env.MONGODB_URI;
-
-if (!uri) {
-  throw new Error('Please define the MONGODB_URI environment variable inside .env');
-}
-
 let cachedClient: MongoClient | null = null;
 let cachedDb: Db | null = null;
 
 export async function connectToDatabase() {
+  const uri = process.env.MONGODB_URI;
+
+  if (!uri) {
+    throw new Error('Please define the MONGODB_URI environment variable inside .env');
+  }
+
   if (cachedClient && cachedDb) {
     return { client: cachedClient, db: cachedDb };
   }
